0b92ee9c49471f44f737b1cba2a49103cf0a8c29
Joshna Lingala Uthama Reddy Lingala <joshnalingala.u@syncfusion.com>
2022-06-06 22:50:34 +0530

    Adding Sample

---
+++ BlazorApp1/Globals.cs
@@ -0,0 +1,16 @@
+﻿
+public class SessionGlobals
+{
+    public String? MainMessage { get; set; } = "Initial Value.";
+
+    public event Action OnChange;
+
+    public void SetMessage(string message)
+    {
+        MainMessage = message;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
+
